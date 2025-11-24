c45778f3ce02fc2596c8100e1bb0f8fd7499f836
mdnoor-0 <md.noor.hassnain.01@gmail.com>
2025-11-25 00:28:10 +0530

    added password length slider and symbol and number toggle

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-let pbox1 = document.querySelector("#pbox1")
-let pbox2 = document.querySelector("#pbox2")
-
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const nonumber = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const nosymbol = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const onlyalpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const len = document.querySelector('#len')
+const sym = document.querySelector('#sym')
+const num = document.querySelector('#num')
+const pbox1 = document.querySelector("#pbox1")
+const pbox2 = document.querySelector("#pbox2")
+let symbol = true
+let number = true
+let length = 8
 let pass1 = ''
 let pass2 = ''
 
 function getRandom(){
-    return Math.floor(Math.random()*characters.length);
+    if(symbol && number) return Math.floor(Math.random()*characters.length);
+    else if(symbol && !number) return Math.floor(Math.random()*nonumber.length);
+    else if(!symbol && number) return Math.floor(Math.random()*nosymbol.length);
+    else return Math.floor(Math.random()*onlyalpha.length);
 }
 function password(){
     let s = ""
-    for(let i = 0;i<15;i++){
+    for(let i = 0;i<length;i++){
         s += characters[getRandom()]
     }
     return s;
@@ -25,18 +34,36 @@ function generate(){
 }
 
 function copy(box){
+    if(pass1=='' || pass2=='') return;
     if(box == '1'){
         navigator.clipboard.writeText(pbox1.textContent);
-        pbox1.textContent = 'Copied✅'
+        pbox1.textContent = 'Copied!'
         setTimeout(() =>{
             pbox1.textContent = pass1;
         }, 1500);
     }
     else{
         navigator.clipboard.writeText(pbox2.textContent);
-        pbox2.textContent = 'Copied✅';
+        pbox2.textContent = 'Copied!';
         setTimeout(() =>{
             pbox2.textContent = pass2;
         }, 1500)
     }
 }
+
+len.addEventListener('input', ()=>{
+    length = len.value;
+    generate()
+});
+
+sym.addEventListener('change',() =>{
+    if(sym.checked) symbol = true;
+    else symbol = false;
+    generate();
+})
+
+num.addEventListener('change', ()=>{
+    if(num.checked) number = true;
+    else number = false;
+    generate();
+})
